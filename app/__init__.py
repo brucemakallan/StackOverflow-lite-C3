9244b226127fbf1b4f047bb45ea125e395e86608
Bruce Makaaru <brucemakallan@gmail.com>
2018-09-11 20:47:07 +0300

    Add CORS management

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 from config import app_config
@@ -7,6 +8,7 @@ from config import app_config
 def create_app(config_name):
     """Create app using configuration name from environment variable: APP_SETTINGS"""
     app = Flask(__name__)
+    CORS(app)  # for Cross Origin Requests
     app.config.from_object(app_config[config_name])
     JWTManager(app)
     return app
